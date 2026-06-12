3bb90e42963c7e6015fa48130c0f263733e081f2
KASMI El Mehdi (INTERN) <EKI7@dsone.3ds.com>
2026-06-12 13:44:39 +0200

    implemented the thread safe map

---
+++ cache.cpp
@@ -0,0 +1,29 @@
+#include <unordered_map>
+#include <string>
+#include <shared_mutex>
+#include <optional>
+
+class safe_cache
+{
+    std::unordered_map<std::string, std::string> cache;
+    // multi readers , one writer thread
+    std::shared_mutex mu_;
+
+public:
+std::optional<std::string> get(std::string key){
+    std::shared_lock lock(mu_);
+    auto it = cache.find(key);
+    if (it == cache.end()) return std::nullopt ;
+    return it->second ;
+    
+}
+    void put(std::string key, std::string val)
+    {
+        // acquiring lock
+        std::shared_lock lock(mu_);
+
+        cache.insert(key, val);
+        // releasing lock lock
+        lock.unlock();
+    }
+};
